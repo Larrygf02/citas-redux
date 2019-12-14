a434c7e1fa9f84bc0c43690cf9bf8c47b0dee0c5
larrygf02 <raulgf_02@hotmail.com>
2019-12-14 10:42:27 -0500

    Borrar cita action redux

--- src/componentes/ListadoCitas.js
+++ src/componentes/ListadoCitas.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { borrarCitaAction } from '../actions/CitasActions'
+import { useDispatch, useSelector } from 'react-redux'
 
 const ListadoCitas = () => {
     // obtener las citas del state
     const citas = useSelector(state => state.citas)
+
+    // Dispatch para eliminar
+    const dispatch = useDispatch()
+    const borrarCita = id => dispatch(borrarCitaAction(id))
 
     //Ver si hay citas
     const mensaje = Object.keys(citas.citas).length === 0 ? 'No hay citas' : 'Administra las Citas aquí'
@@ -13,7 +18,7 @@ const ListadoCitas = () => {
             <h2 className="card-title text-center">{mensaje}</h2>
                 <div className="lista-citas">
                     {citas.citas.map(cita => (
-                        <div className="media mt-3">
+                        <div key={cita.id} className="media mt-3">
                             <div className="media-body">
                                 <h3 className="mt-0">{cita.mascota}</h3>
                                 <p className="card-text"><span>Nombre Dueño: {cita.propietario}</span></p>
@@ -21,7 +26,7 @@ const ListadoCitas = () => {
                                 <p className="card-text"><span>Hora: {cita.hora}</span>  </p>
                                 <p className="card-text"><span>Sintomas: {cita.sintomas}</span> <br /> </p>
                                 <button 
-                                    className="btn btn-danger">Borrar &times;
+                                    className="btn btn-danger" onClick={() => borrarCita(cita.id)}>Borrar &times;
                                 </button>
                             </div>
                         </div>
